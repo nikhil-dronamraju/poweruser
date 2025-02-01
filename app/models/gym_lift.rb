e1d160f0390774e7c6e69ccb580460afafd28e3a
Nikhil Dronamraju <nikhil.dronamraju.0917@gmail.com>
2025-02-01 17:07:31 -0600

    Fixed error messages

--- app/models/gym_lift.rb
+++ app/models/gym_lift.rb
@@ -1,7 +1,7 @@
 class GymLift < ApplicationRecord
   belongs_to :exercise
   belongs_to :workout
-  validates :reps, numericality: { greater_than_or_equal_to: 1, message: "This shit is off." }
-  validates :sets, numericality: { greater_than_or_equal_to:  1, message: "at least one set!" }
-  validates :weight, numericality: { greater_than_or_equal_to: 1, message: "at least one pound!" }
+  validates :reps, numericality: { greater_than_or_equal_to: 1, message: "ERROR: Must have at least one rep" }
+  validates :sets, numericality: { greater_than_or_equal_to:  1, message: "ERROR: Must have at least one set" }
+  validates :weight, numericality: { greater_than_or_equal_to: 1, message: "ERROR: Must have at least one pound" }
 end
