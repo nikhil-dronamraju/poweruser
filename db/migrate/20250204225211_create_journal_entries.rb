884c8d54e327942a74a9f66c6be99d5045f1594f
Nikhil Dronamraju <nikhil.dronamraju.0917@gmail.com>
2025-02-06 17:16:33 -0600

    Uninstalled actiontext

--- db/migrate/20250204225211_create_journal_entries.rb
+++
@@ -1,7 +0,0 @@
-class CreateJournalEntries < ActiveRecord::Migration[7.2]
-  def change
-    create_table :journal_entries do |t|
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,47 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_05_014512) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_06_225458) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "action_text_rich_texts", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "body"
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
-  end
-
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
 
   create_table "exercises", force: :cascade do |t|
     t.text "title"
@@ -75,14 +37,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_05_014512) do
     t.index ["workout_id"], name: "index_gym_lifts_on_workout_id"
   end
 
-  create_table "journal_entries", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "title"
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_journal_entries_on_user_id"
-  end
-
   create_table "sagas", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "title"
@@ -95,18 +49,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_05_014512) do
 
   create_table "tracks", force: :cascade do |t|
     t.string "title"
-    t.string "icon_class"
+    t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "tracks_users", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "track_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["track_id"], name: "index_tracks_users_on_track_id"
-    t.index ["user_id"], name: "index_tracks_users_on_user_id"
   end
 
   create_table "user_tracks", force: :cascade do |t|
@@ -137,14 +82,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_05_014512) do
     t.index ["user_id"], name: "index_workouts_on_user_id"
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "gym_lifts", "exercises"
   add_foreign_key "gym_lifts", "workouts"
-  add_foreign_key "journal_entries", "users"
   add_foreign_key "sagas", "users"
-  add_foreign_key "tracks_users", "tracks"
-  add_foreign_key "tracks_users", "users"
   add_foreign_key "user_tracks", "tracks"
   add_foreign_key "user_tracks", "users"
   add_foreign_key "workouts", "users"
