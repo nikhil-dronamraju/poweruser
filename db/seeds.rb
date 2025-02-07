3316eb0e32e24dc8d9ae75d37c5fce9a06d3e323
Nikhil Dronamraju <nikhil.dronamraju.0917@gmail.com>
2025-02-07 17:29:56 -0600

    Seeding improvements

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,6 @@ csv.each do |row|
   t.save
   puts "#{t.title} saved"
 end
-password_digest = BCrypt::Password.create('password')
 
 Track.create(title: "Physical Health", icon: '<i class="fa-solid fa-dumbbell"></i>')
 Track.create(title: "Social Life", icon: '<i class="fa-solid fa-person"></i>')
@@ -37,5 +36,5 @@ Track.create(title: "Professional", icon: '<i class="fa-solid fa-user-tie"></i>'
 Track.create(title: "Creative", icon: '<i class="fa-solid fa-paintbrush"></i>')
 Track.create(title: "Quitting bad habits", icon: '<i class="fa-solid fa-syringe"></i>')
 
-User.create!(name: "Test User", password_digest: password_digest, username: "testu1", tracks: [ Track.first ])
+User.create!(name: "Test User", password: "password", username: "testu1", tracks: [ Track.first ])
 
