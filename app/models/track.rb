9bc518110f16518bbe4c42676d27a99b10691800
Nikhil Dronamraju <nikhil.dronamraju.0917@gmail.com>
2025-02-15 18:24:39 -0800

    Planned approach and deleted unneeded file

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,7 +1,6 @@
 class Track < ApplicationRecord
   has_many :users, through: :user_tracks
   has_many :user_tracks, dependent: :destroy
-
   validates :title, presence: { message: "Must have a title." }
   validates :icon, presence: { message: "Must have a icon." }
 end
