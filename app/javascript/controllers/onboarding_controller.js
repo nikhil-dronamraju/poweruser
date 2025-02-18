77749a814065558ef20ecb840fb05dc11cc384b5
Nikhil Dronamraju <nikhil.dronamraju.0917@gmail.com>
2025-02-18 03:23:01 -0800

    Fixed js level bug

--- app/javascript/controllers/onboarding_controller.js
+++ app/javascript/controllers/onboarding_controller.js
@@ -7,7 +7,7 @@ export default class extends Controller {
       const buttonElement = this.element;
       const index = this.data.get("index");
       const icon = buttonElement.children[0];
-      const inputHiddenField = document.getElementById(`user_track_${index}_icon`);
+      const inputHiddenField = document.getElementById(`user_tracks_attributes_${index}_icon`);
       const openModalButton = document.getElementById(`open_icon_modal_${index}`);
       openModalButton.innerHTML = icon.outerHTML;
       inputHiddenField.value = icon.outerHTML;
