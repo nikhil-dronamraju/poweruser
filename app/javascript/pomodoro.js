04de2a49d037c6b64968393b4333ec9874fe85fd
Nikhil Dronamraju <nikhil.dronamraju.0917@gmail.com>
2025-02-26 02:42:04 -0800

    Researching approaches to render timer across all the pages

--- app/javascript/pomodoro.js
+++ app/javascript/pomodoro.js
@@ -1,41 +1,35 @@
+// let pomodoroTimes = [ 1500, 300, 1500, 300, 1500, 300, 1500, 300, 1800 ];
+// let selectedIndex = 0;
+// let timeElapsed = 0;
+// let seconds = pomodoroTimes[ selectedIndex ];
+// let pomodoroInterval;
+// let worker;
+//
+// function windClock(){
+//     seconds -= 1;
+//     const timerElement = document.getElementById("pomodoro_time");
+//     if ( seconds === 0 ) {
+//         timerElement.classList.remove("has-text-primary");
+//         timerElement.classList.add("has-text-danger");
+//         selectedIndex += 1;
+//         stopPomodoroTimer();
+//     } else {
+//         timerElement.classList.remove("has-text-danger");
+//         timerElement.classList.add("has-text-primary");
+//     }
+//
+//     if ( selectedIndex >= pomodoroTimes.length ) {
+//         selectedIndex = 0;
+//     }
+//
+//     // UI logic:
+//     timerElement.innerText = formatSeconds(seconds);
+// }
+//
+
+// Default pomodoro intervals:
 let pomodoroTimes = [ 1500, 300, 1500, 300, 1500, 300, 1500, 300, 1800 ];
-let selectedIndex = 0;
-let timeElapsed = 0;
-let seconds = pomodoroTimes[ selectedIndex ];
-let pomodoroInterval;
-
-/**
- * We need an accurate pomodoro timer - The first thing is to have it go from 25:00 to 0:00
- * In seconds that's 1500 to 0.
- * Done - We also need to do some code cleanup.
- * Yo, this IDE is in my way. Basically - format seconds
- * So - We can just use this array indefinitely
- * Anyways, we start off at arr[0]
- *
- */
-
-
-
-function windClock(){
-    seconds -= 1;
-    const timerElement = document.getElementById("pomodoro_time");
-    if ( seconds === 0 ) {
-        timerElement.classList.remove("has-text-primary");
-        timerElement.classList.add("has-text-danger");
-        selectedIndex += 1;
-        stopPomodoroTimer();
-    } else {
-        timerElement.classList.remove("has-text-danger");
-        timerElement.classList.add("has-text-primary");
-    }
-
-    if ( selectedIndex >= pomodoroTimes.length ) {
-        selectedIndex = 0;
-    }
-
-    // UI logic:
-    timerElement.innerText = formatSeconds(seconds);
-}
+let i = 0;
 
 function formatSeconds( numberOfSeconds ) {
     let minutes = parseInt(numberOfSeconds / 60);
@@ -43,11 +37,11 @@ function formatSeconds( numberOfSeconds ) {
     return `${minutes}:${seconds}`;
 }
 
-function startPomodoroTimer(){
-    pomoInterval = setInterval(windClock, 1000);
+function timedCount() {
+    i = i + 1;
+    postMessage(i);
+    setTimeout("timedCount()",1000);
 }
 
-function stopPomodoroTimer() {
-    clearInterval(pomoInterval);
-}
+timedCount();
 
