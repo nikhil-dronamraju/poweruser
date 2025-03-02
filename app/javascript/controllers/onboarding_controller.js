d4daa40346a50e30ff85bd410b0964315c53056f
Nikhil Dronamraju <nikhil.dronamraju.0917@gmail.com>
2025-03-02 01:33:28 -0800

    Lots of stimulus improvements

--- app/javascript/controllers/onboarding_controller.js
+++ app/javascript/controllers/onboarding_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["generic", "track", "saga"];
+  static targets = [ "username", "name", "password", "passwordConfirmation", "email", "usernameError", "nameError", "passwordError", "passwordConfirmationError", "emailError" ];
   static values = {
       currentPage: String,
       nextPage: String
@@ -17,10 +17,62 @@ export default class extends Controller {
   }
 
   flip_to_page(){
-    let currentPage = document.getElementById(this.currentPageValue);
-    let nextPage = document.getElementById(this.nextPageValue);
-    currentPage.classList.add("display-none");
-    nextPage.classList.remove("display-none");
+    let inputsAndErrIcons = new Map();
+    inputsAndErrIcons.set(this.usernameTarget, this.usernameErrorTarget);
+    inputsAndErrIcons.set(this.nameTarget, this.nameErrorTarget );
+    inputsAndErrIcons.set(this.emailTarget, this.emailErrorTarget);
+    inputsAndErrIcons.set(this.passwordTarget, this.passwordErrorTarget);
+    inputsAndErrIcons.set(this.passwordConfirmationTarget, this.passwordConfirmationErrorTarget);
+    let checkBlankValidations = this.validateBlanks(inputsAndErrIcons);
+    let checkPasswordMatches = this.checkPasswordMatches();
+    let checkIsEmail = this.checkEmailValid();
+    if (checkBlankValidations && checkPasswordMatches && checkIsEmail) {
+        // Flip to the next page?
+        if (this.currentPageValue === "generic"){
+            this.element.classList.add("display-none");
+
+        }
+    }
   }
+
+  validateBlanks(fieldMap) {
+      fieldMap.keys().forEach((k) => {
+          let isBlank = this.checkIsBlank(k, fieldMap.get(k));
+          if (isBlank) {
+              return false;
+          }
+      });
+      return true;
+  }
+
+  checkIsBlank(field, fieldErrIcon) {
+      let targetName = field.dataset.onboardingTarget;
+      let capitalizedTargetName = String(targetName[0]).toUpperCase() + String(targetName).slice(1);
+      if (capitalizedTargetName === "PasswordConfirmation") {
+          capitalizedTargetName = "Password confirmation"
+      }
+      let isBlank = field.value === ""
+      if (isBlank) {
+          fieldErrIcon.classList.remove("display-none");
+          field.placeholder = `${capitalizedTargetName} cannot be blank`;
+          return true;
+      } else {
+          fieldErrIcon.classList.add("display-none");
+          return false;
+      }
+  }
+
+  checkPasswordMatches() {
+      return this.passwordTarget.value === this.passwordConfirmationTarget.value;
+  }
+
+  checkEmailValid() {
+      const isEmail = /^.+@.+$/;
+      const regex = new RegExp(isEmail);
+      return regex.test(this.emailTarget.value);
+  }
+
+
+
 
 }
