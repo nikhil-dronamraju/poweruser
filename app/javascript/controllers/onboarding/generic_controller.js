836e2fb779983e301b3a08a910dcc761299f2753
Nikhil Dronamraju <nikhil.dronamraju.0917@gmail.com>
2025-03-02 17:34:56 -0800

    Used vanilla js, because stimulus is annoying (but more onboarding improvements

--- app/javascript/controllers/onboarding/generic_controller.js
+++ app/javascript/controllers/onboarding/generic_controller.js
@@ -6,15 +6,6 @@ export default class extends Controller {
       currentPage: String,
       nextPage: String
   }
-  update_icon_input() {
-      const buttonElement = this.element;
-      const index = this.data.get("index");
-      const icon = buttonElement.children[0];
-      const inputHiddenField = document.getElementById(`user_tracks_attributes_${index}_icon`);
-      const openModalButton = document.getElementById(`open_icon_modal_${index}`);
-      openModalButton.innerHTML = icon.outerHTML;
-      inputHiddenField.value = icon.outerHTML;
-  }
 
   flip_to_page(){
     let inputsAndErrIcons = new Map();
